2d2a8772d3183693156d524b0deb60ddc50008e8
Mudit Ameta <zeusdeux@gmail.com>
2019-01-09 18:42:36 +0100

    Inject components into MDX so that no imports are needed

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,21 +3,19 @@ import PropTypes from 'prop-types'
 
 import { graphql } from 'gatsby'
 import MDXRenderer from 'gatsby-mdx/mdx-renderer'
+import { withMDXScope } from 'gatsby-mdx/context'
 
-// import { withMDXScope } from 'gatsby-mdx/context'
+import Layout from '../components/layout'
+import Video from '../components/video'
 
 function PostPageTemplate({ data: { mdx } }) {
-  return (
-    <article>
-      <MDXRenderer>{mdx.code.body}</MDXRenderer>
-    </article>
-  )
+  return <MDXRenderer scope={{ Layout, Video }}>{mdx.code.body}</MDXRenderer>
 }
 
 PostPageTemplate.propTypes = {
   data: PropTypes.object
 }
-export default PostPageTemplate
+export default withMDXScope(PostPageTemplate)
 
 export const pageQuery = graphql`
   query($id: String!) {
